12381b23999f2ed192b76f68a3eabbb193e81785
Punyawat.S <99156196+PNYWT@users.noreply.github.com>
2024-03-19 09:13:56 +0700

    first commit

---
+++ Detect-spam-text-using-NLP-in-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detect-spam-text-using-NLP-in-Swift
+//
+//  Created by Dev on 19/3/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
